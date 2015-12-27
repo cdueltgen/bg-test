c2d8cb899d6a7200fa13b4400adacc222d960f7c
cdueltgen <cdueltgen@gmail.com>
2015-12-26 18:21:08 -0800

    s3 upload working

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug import secure_filename
+import boto3
 
-# pull debug True/False from env?
 UPLOAD_FOLDER = '/tmp'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+DEBUG = "NO_DEBUG" not in os.environ
+# AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+# AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+S3_BUCKET = os.environ.get('S3_BUCKET')
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def allowed_file(filename):
@@ -28,12 +32,17 @@ def get_upload():
 @app.route('/upload', methods=["POST"])
 def upload_file():
     f = request.files['file']
+    file_contents = f.read()
     if f and allowed_file(f.filename):
         filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        s3_bucket = os.environ.get('S3_BUCKET')
+        s3 = boto3.resource('s3')
+        s3.Bucket(s3_bucket).put_object(Key=filename, Body=file_contents)
+        # This was for testing locally
+        # f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return render_template('success.html', filename=filename)
     else:
         return render_template('failure.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=DEBUG)
