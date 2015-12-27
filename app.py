9ebc2f4255642ce4263f8c94bb97081b7fc8a435
cdueltgen <cdueltgen@gmail.com>
2015-12-27 10:08:32 -0800

    clean up requirements, pep8, docstrings

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 import os
+
+import boto3
 from flask import Flask, render_template, request, redirect
 from werkzeug import secure_filename
-import boto3
 from rq import Queue
 from worker import conn
+
 from utils import upload_queue
 
-# UPLOAD_FOLDER = '/tmp'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 DEBUG = "NO_DEBUG" not in os.environ
 S3_BUCKET = os.environ.get('S3_BUCKET')
 PORT = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def allowed_file(filename):
+    """Determine if the uploaded file is an uploadable type."""
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
+# Moved to utils.py
 # def upload_queue(filename, file_contents):
 #     s3 = boto3.resource('s3')
 #     s3.Bucket(S3_BUCKET).put_object(Key=filename, Body=file_contents)
@@ -29,16 +30,19 @@ def allowed_file(filename):
 
 @app.route('/')
 def index():
+    """Landing page."""
     return render_template("hello.html")
 
 
 @app.route('/upload_form')
 def get_upload():
+    """Render the upload form."""
     return render_template("upload_form.html")
 
 
 @app.route('/upload', methods=["GET", "POST"])
 def upload_file():
+    """Read the file and put upload in worker queue."""
     if request.method == "POST":
         f = request.files['file']
         file_contents = f.read()
@@ -46,11 +50,9 @@ def upload_file():
             filename = secure_filename(f.filename)
             q = Queue(connection=conn)
             q.enqueue(upload_queue, filename, file_contents)
-            # upload_queue(filename, file_contents)
+            # moved to utils.py
             # s3 = boto3.resource('s3')
             # s3.Bucket(S3_BUCKET).put_object(Key=filename, Body=file_contents)
-            # This was for testing locally
-            # f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return render_template('success.html', filename=filename)
         else:
             return render_template('failure.html')
@@ -60,6 +62,7 @@ def upload_file():
 
 @app.route('/bucketlist')
 def list_bucket():
+    """Ask s3 for a list of things in the bucket."""
     s3 = boto3.resource('s3')
     objects = s3.Bucket(S3_BUCKET).objects.all()
     return render_template('bucketlist.html', objects=objects)
--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
-import boto3
 import os
+
+import boto3
 
 S3_BUCKET = os.environ.get('S3_BUCKET')
 
 
 def upload_queue(filename, file_contents):
+    """Upload to s3 function for use with the worker queue."""
+    # file_contents = f.read()
+
     s3 = boto3.resource('s3')
     s3.Bucket(S3_BUCKET).put_object(Key=filename, Body=file_contents)
     return
